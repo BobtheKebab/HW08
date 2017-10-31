623af4889d83ad0cba9827f0891eb8d4ddea4211
Ahbab Ashraf <ahbab.ashraf@gmail.com>
2017-10-31 16:56:25 -0400

    permHelp is done

--- work08.c
+++ work08.c
@@ -4,6 +4,46 @@
 #include <time.h>
 #include <sys/stat.h>
 
+
+char * perm (mode_t mode) {
+  int num = mode % 512;
+  
+}
+
+char * permHelp (int num) {
+  char ans[3];
+  int count = 2;
+  char p;
+  while (count >= 0) {
+    if (num - power(2, count) > -1) {
+      num -= power(2, count);
+      if (count == 2) { p = 'r'; }
+      else if (count == 1) { p = 'w'; }
+      else { p = 'x'; }
+      ans[2-count] = p;
+    } else {
+      ans[2-count] = '-';
+    }
+    count--;
+  }
+  return ans;
+}
+
+int power(int x, int y) {
+  int ans = 1;
+  for (y; y; y--) {
+    ans *= x;
+  }
+  return ans;
+}
+
+char * format(int ary[]) {
+  int count = 0;
+  while (count < 3) {
+    
+  }
+}
+
 int main() {
 
   struct stat yoshi;
@@ -16,6 +56,9 @@ int main() {
   
   printf("File mode: %o\n", f_size);
   printf("Last access: %s\n", ctime( &yoshi.st_atime ));
-  
+  //  printf("File permissions in ls-l format: %s\n", perm(yoshi.st_mode));
+  //  printf("%d\n", power(2, 2));
+  printf("permissions 4: %s\n", permHelp(4));
+  printf("permissions 6: %s\n", permHelp(1));
 }
 
